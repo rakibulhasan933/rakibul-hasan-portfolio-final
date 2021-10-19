9edcbf56c3dd7086bfb6220ae6f3fbc1ba271fee
rakibulhasan933 <rakibulmd933@gmail.com>
2021-10-19 15:48:42 +0600

    testimonial and footer

--- src/components/Contract/Contract.jsx
+++ src/components/Contract/Contract.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contract.scss'
 
 export default function Contract() {
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
     return (
         <div className="contract" id="contract">
-
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Submit</button>
+                    {message && <span>Thanks,I'll reply ASSP:)</span>}
+                </form>
+            </div>
         </div>
     )
 }
